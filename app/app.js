fb22c3fd9d7f99c383bb951b3838e1b5edd8a0ef
Nazim <developernazim.dev@gmail.com>
2025-05-21 19:04:14 +0600

    Add openapispecs documentation

--- app/app.js
+++ app/app.js
@@ -1,16 +1,21 @@
 const express = require("express");
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
 const middleware = require("./middlewares");
-const router = require('./router')
+const router = require("./router");
 const { notFoundError, serverError } = require("./error");
 
 const app = express();
 
+// Serving the yaml documents
+const swaggerDocs = YAML.load("openapi.yaml");
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 // Using middlewares
 app.use(middleware);
 
 //Using routes middleware
-app.use('/api/v1/', router)
+app.use("/api/v1/", router);
 
 // Using error middlewares
 app.use(notFoundError, serverError);
